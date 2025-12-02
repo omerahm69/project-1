dc1f5ee54337e7e3477b886c2f5f1c473f3a917f
omer ahmed <omerahm69@gmail.com>
2025-12-02 18:25:31 +0100

    fixed languge selection

--- translate.js
+++ translate.js
@@ -14,8 +14,9 @@ async function setLanguage(lang) {
     // Replace all data-i18n elements
     document.querySelectorAll("[data-i18n]").forEach(el => {
       const key = el.getAttribute("data-i18n");
-      const text = key.split(".").reduce((obj, i) => obj[i], translations);
-      el.innerHTML = text;
+      const text = key.split(".").reduce((obj, i) => (obj ? obj[i] : undefined), translations) || "";
+el.innerHTML = text;
+      
     });
 
     // RTL support
@@ -23,12 +24,10 @@ async function setLanguage(lang) {
 
     // Save preference
     localStorage.setItem("lang", lang);
-
   } catch (error) {
     console.error("Error loading translations:", error);
   }
 }
-
 // Called when user selects a language
 function changeLanguage(lang) {
   setLanguage(lang);
@@ -36,4 +35,3 @@ function changeLanguage(lang) {
 
 // Load selected language at startup
 window.onload = () => setLanguage(currentLang);
-
